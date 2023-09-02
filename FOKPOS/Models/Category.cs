cb472ebf4d194824f9bb72bc339ffd9b9d86bb9a
BraQobiGH <egg_bone16@yahoo.com>
2023-09-02 21:34:27 +0000

    first push

---
+++ FOKPOS/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FOKPOS.Models
+{
+    public class Category
+    {
+        [Required]
+        public string catId { get; set; }
+        [Required]
+        public string catName { get; set; }
+    }
+}
---
+++ FOKPOS/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿
+    $(document).ready(function() {
+  // Get references to the sidebar and sidebar items
+  const firstSidebar = $("#sidebar");
+  const sidebarItems = firstSidebar.find(".list-unstyled.components > li > a");
+
+    // Add click event handler to sidebar items
+    sidebarItems.click(function(event) {
+        event.preventDefault(); // Prevent default link behavior
+    const targetSidebar = $("#sidebar2");
+
+    // Toggle the visibility of the second sidebar
+    targetSidebar.toggleClass("active");
+
+    // Close the second sidebar if it's already open and a different item is clicked
+    if (targetSidebar.hasClass("active") && !$(this).hasClass("active")) {
+        targetSidebar.removeClass("active");
+    }
+
+    // Remove the "active" class from other sidebar items and add it to the clicked item
+    sidebarItems.removeClass("active");
+    $(this).addClass("active");
+  });
+});
+
